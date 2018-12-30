72167beacd3c44cc90256d807ab1d3bd86cbe50c
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2018-12-30 21:01:44 +0100

    Added model Vehicle

--- book/models.py
+++ book/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Vehicle(models.Model):
+    number = models.CharField(max_length=10)
+    vehicle_type = models.CharField(max_length=10,
+                                    db_index=True,)
+    slug = models.SlugField(max_length=20,
+                            db_index=True,
+                            unique=True)
+    trolleys = models.CharField(max_length=10,
+                                unique=True)
+    warranty = models.DateField()
+
+    def __str__(self):
+        return 'Pojazd: {}-{}'.format(self.vehicle_type, self.number)
+
+    def get_absolute_url(self):
+        return reverse('book:vehicle_detail',
+                       args=[self.id, self.slug])
+
 
 # Create your models here.
