f5cbffb7394619205798eb0f0d8b03bf6a3a57e7
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2018-12-30 21:09:17 +0100

    Added model Vehicle to admin page

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Vehicle
 
-# Register your models here.
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('number', 'vehicle_type', 'slug', 'trolleys', 'warranty')
+    list_filter = ('vehicle_type',)
+    prepopulated_fields = {'slug': ('vehicle_type', 'number')}
+
+admin.site.register(Vehicle, VehicleAdmin)
+
