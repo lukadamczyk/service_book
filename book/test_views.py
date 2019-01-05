5b96a20857b4a29404baf2711334c1edc1fe3fa0
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2019-01-05 19:27:32 +0100

    Added test for vehicle view

--- book/test_views.py
+++ book/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from .test_models import create_vehicle, create_owner
+from .models import Owner
 
 
 class HomeViewTestcase(TestCase):
@@ -15,3 +16,19 @@ class HomeViewTestcase(TestCase):
         self.assertEqual(response.context['title'], 'Książka serwisowa')
         self.assertQuerysetEqual(response.context['owners'], ['<Owner: Koleje Wielkopolskie>'])
         self.assertContains(response, 'Koleje Wielkopolskie')
+
+
+class VehicleViewTestCase(TestCase):
+
+    def setUp(self):
+        owner = create_owner(name='Koleje Dolnośląskie', slug='koleje-dolnośląskie')
+        create_vehicle(owner, slug='SA132-001', number='001', vehicle_type='SA132')
+
+    def test_vehicle_list_view(self):
+        owner = Owner.objects.get(id=1)
+        response = self.client.get(reverse('book:vehicle_list', args=['koleje-dolnośląskie']))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book/vehicle/list.html')
+        self.assertEqual(response.context['title'], 'Koleje Dolnośląskie')
+        self.assertEqual(response.context['owner'], owner)
+        self.assertContains(response, 'Pojazd: SA132-001')
