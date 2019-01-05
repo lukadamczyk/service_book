8fea529a71a792cacfcd5444369a812911f4d403
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2019-01-05 17:44:28 +0100

    Added view for home page

--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Owner
 
-# Create your views here.
+
+def home(request):
+    owners = Owner.objects.all()
+    return render(request,
+                  template_name='book/index.html',
+                  context={'title': 'Książka serwisowa',
+                           'owners': owners})
+
