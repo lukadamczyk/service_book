7b68b695db3bf3ae32be8d920323c523563697e8
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2019-01-09 17:03:20 +0100

    Added inspection_view in urls

--- book/urls.py
+++ book/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('complaint/<int:id>/', views.complaint_detail, name='complaint_detail'),
     path('fault/<int:id>/', views.fault_detail, name='fault_detail'),
     path('fault/', views.fault_list, name='fault_list'),
+    path('inspection/', views.inspection_list, name='inspection_list'),
 ]
