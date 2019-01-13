2ad5b8d1e5224a93e79ed267a39713c32c6c1a95
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2019-01-13 15:28:25 +0100

    Added default authentication
    - added decorator login_required to views
    - added login to tests
    - added login in templates

--- service_book/urls.py
+++ service_book/urls.py
@@ -21,5 +21,6 @@ import debug_toolbar
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('book.urls')),
 ]
