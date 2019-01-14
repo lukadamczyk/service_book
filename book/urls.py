1f1d967b3f560cd5b057be82813cadc045dd90f4
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2019-01-14 21:30:16 +0100

    Added link for add new complaint

--- book/urls.py
+++ book/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('complaint/', views.complaint_list, name='complaint_list'),
     path('complaint/<int:id>/', views.complaint_detail, name='complaint_detail'),
     path('fault/<int:id>/', views.fault_detail, name='fault_detail'),
+    path('complaint/add/', views.add_complaint, name='add_complaint'),
     path('fault/', views.fault_list, name='fault_list'),
     path('inspection/', views.inspection_list, name='inspection_list'),
     path('inspection/<int:id>/', views.inspection_detail, name='inspection_detail'),
