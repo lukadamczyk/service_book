49e8758895c450aac16ea8b49cc96bba11218f1e
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2020-06-26 19:46:18 +0200

    Modified secret_key path

--- service_book/settings.py
+++ service_book/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xnn(p*-qog14)cedr!ne7tt*b9$&s-4v#qo$3jd#ik3i@%iovm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
