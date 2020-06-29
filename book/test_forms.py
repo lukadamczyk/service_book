0e73601e9698f378d3081e04a20f68906fa08d48
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2020-06-29 09:30:53 +0200

    Added tests for edit FaultForm

--- book/test_forms.py
+++ book/test_forms.py
@@ -1,9 +1,9 @@
 import datetime
 
 from django.test import TestCase
-from .forms import AddComplaintForm, EditComplaintForm
+from .forms import AddComplaintForm, EditComplaintForm, AddFaultForm
 from .models import Vehicle, Trolleys, Owner
-from .test_models import create_vehicle, create_owner, create_trolleys
+from .test_models import create_vehicle, create_owner, create_trolleys, create_complaint, create_fault
 
 
 today = datetime.date.today()
@@ -20,6 +20,21 @@ def create_form(doc, date, status, vehicle, form):
     }
     add_form = form(data)
     return add_form
+
+def create_form_falut(name, category, description, action, comments, zr, status, end_date, need, form):
+    data = {
+        'name': name,
+        'category': category,
+        'description': description,
+        'actions': action,
+        'comments': comments,
+        'zr_number': zr,
+        'status': status,
+        'end_date': end_date,
+        'need': need
+    }
+    fault_form = form(data)
+    return fault_form
 
 class AddComplainFormTestCase(TestCase):
     def setUp(self):
@@ -60,3 +75,20 @@ class AddComplainFormTestCase(TestCase):
         self.assertFalse(test_form.is_valid())
         test_form = create_form('Kw23', yesterday, 'open', 3, AddComplaintForm)
         self.assertFalse(test_form.is_valid())
+
+
+class AddFaultFormTestCase(TestCase):
+    def setUp(self):
+        owner = create_owner()
+        trolleys = create_trolleys(name='sa123', first='123', second='234')
+        vehicle = create_vehicle(trolleys, owner, '001', 'SA132', 'sa132-001')
+        complaint = create_complaint(vehicle=vehicle, client=owner)
+
+    def test_valid_form_fault(self):
+        test_form = create_form_falut('usterka silnika', 'podłoga', 'test', '', '', '', 'open', '', '', AddFaultForm)
+        self.assertTrue(test_form.is_valid())
+
+    def test_invalid_form_falut_name(self):
+        test_form = create_form_falut('kw123', 'podłoga', 'test', '', '', '', 'open', '', '', AddFaultForm)
+        self.assertTrue(test_form.is_valid())
+
