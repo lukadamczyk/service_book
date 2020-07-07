2c971a6bfcdebe754513261938796d80bfbf0d67
Łukasz Adamczyk <lukaszadamczyk@mail.com>
2020-07-07 22:02:24 +0200

    Modified urls for change password view

--- service_book/urls.py
+++ service_book/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from book import views
 
 import debug_toolbar
 
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('change_password/', views.CustomPasswordChangeView.as_view(),
+         name='change_password'),
+    path('change_password_done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='registration/change_password_done.html'),
+         name='password_change_done'),
     path('', include('book.urls')),
 ]
